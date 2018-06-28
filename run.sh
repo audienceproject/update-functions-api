d1b64a96b1fd8bb0afca96114fa57ffd52181d64
Eric Eijkelenboom <ee@userreport.com>
2018-06-28 13:47:14 +0200

    Clearly echoing something is not enough

--- run.sh
+++ run.sh
@@ -15,9 +15,9 @@ type_files=WERCKER_UPDATE_FUNCTIONS_API_TYPES
 # Update Functions API with each Type in the list of type files
 for type_file in `echo $type_files | tr "," "\n"`; do
     type_name=$(basename "$type_file" .json)
-    echo curl -H "Auth: Bearer $api_oauth_token" -H "Content-Type: application/json" -X PUT -d @$type_file $api_endpoint/types/$type_name
+    curl -H "Auth: Bearer $api_oauth_token" -H "Content-Type: application/json" -X PUT -d @$type_file $api_endpoint/types/$type_name
 done
 
 # Update Function API with function
 function_name=$(basename "$function_file" .json)
-echo curl -H "Auth: Bearer $api_oauth_token" -H "Content-Type: application/json" -X PUT -d @$function_file $api_endpoint/functions/$function_name
+curl -H "Auth: Bearer $api_oauth_token" -H "Content-Type: application/json" -X PUT -d @$function_file $api_endpoint/functions/$function_name
